b9bbdbcb8d4bacb88cc072e04597cd540e3369da
Marina Lee <leemarina@gmail.com>
2021-07-14 15:40:56 -0700

    button animation

--- aboutme/ViewController.swift
+++ aboutme/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var imageButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+       
 
+    }
+   // var active = true
+    
+     let images = [UIImage(named: "new pfp 1"), UIImage(named: "IMG_0839 1"), UIImage(named: "IMG_0618 1")]
+    var count = 0
+    
+    
+    @IBAction func imageClicked(_ sender: UIButton) {
+   
+        imageButton.setImage(images[Int.random(in: 0...2)], for: .normal)
+        UIView.animate(withDuration: 0.15, delay: 0, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.5, options: .curveEaseIn, animations: {
+            self.imageButton.transform = CGAffineTransform(scaleX: 0.92, y: 0.92)
+        }) { (_) in
+            UIView.animate(withDuration: 0.15, delay: 0, usingSpringWithDamping: 0.4, initialSpringVelocity: 2, options: .curveEaseIn, animations: {
+                self.imageButton.transform = CGAffineTransform(scaleX: 1, y: 1)
+            }, completion: nil)
+        }
+        
+    }
+    
+   
+   
 
 }
 
